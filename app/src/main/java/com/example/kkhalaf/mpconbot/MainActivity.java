30ec8ad071a7ee7717329304209424720a9d74b3
KKLTU <kkhalaf@ltu.edu>
2016-02-05 16:28:41 -0500

    removed all codes. Need to use AsyncTask

--- app/src/main/java/com/example/kkhalaf/mpconbot/MainActivity.java
+++ app/src/main/java/com/example/kkhalaf/mpconbot/MainActivity.java
@@ -5,8 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import java.io.*;
-import java.net.*;
+import java.net.DatagramSocket;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,56 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Button Declaration
-        Button ButtonOne = (Button) findViewById(R.id.Test);
+        Button ButtonOne = (Button) findViewById(R.id.TestButton);
 
-        // Test Button
-        ButtonOne.setOnClickListener
-                (
-                        new Button.OnClickListener()
-                        {
-                            public void onClick(View v)
-                            {
-//                                String host = "127.0.0.1"; // localhost
-//                                // String host = "198.111.37.84";
-//                                int port = 15000;
-//                                String message = "Test";
-//                                DatagramSocket dsocket = null;
-//
-//                                try {
-//                                    // Get the Internet address of the specified host
-//                                    InetAddress address = InetAddress.getByName(host);
-//
-//                                    // Initialize a datagram packet with data and address
-//                                    DatagramPacket packet = new DatagramPacket(
-//                                            message.getBytes(),
-//                                            message.length(),
-//                                            address, port);
-//
-//                                    // Create a datagram socket, send the packet through it, close it.
-//                                    dsocket = new DatagramSocket();
-//                                    dsocket.send(packet);
-//                                    dsocket.close();
-//                                } catch (Exception e) {
-//                                    e.printStackTrace();
-//                                }
-                                String udpMsg = "hello world from UDP client";
-                                DatagramSocket ds = null;
-                                try {
-                                    ds = new DatagramSocket();
-                                    InetAddress serverAddr = InetAddress.getByName("127.0.0.1");
-                                    DatagramPacket dp;
-                                    dp = new DatagramPacket(udpMsg.getBytes(), udpMsg.length(), serverAddr, 15000);
-                                    ds.send(dp);
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-//                                } finally {
-//                                    if (ds != null) {
-//                                        ds.close();
-//                                    }
-                                }
+        // Button Click
+        ButtonOne.setOnClickListener(
+                        new Button.OnClickListener() {
+                            public void onClick(View v) {
+                                TextView TextOne = (TextView) findViewById(R.id.TestText);
+                                TextOne.setText("Hi");
+                                
                             }
                         }
                 );
-
     }
+
 }
+
