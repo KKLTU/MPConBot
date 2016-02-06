8cba955a846501c41053b67ff5e5577f551487ed
KKLTU <kkhalaf@ltu.edu>
2016-02-06 01:01:59 -0500

    Used StrictMode. got rid of the Android.os.NetworkOnMainThreadException Error but App is not working

--- app/src/main/java/com/example/kkhalaf/mpconbot/MainActivity.java
+++ app/src/main/java/com/example/kkhalaf/mpconbot/MainActivity.java
@@ -5,8 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import java.net.DatagramSocket;
-
+import java.net.*;
+import android.os.StrictMode;
+import android.os.AsyncTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +25,36 @@ public class MainActivity extends AppCompatActivity {
                             public void onClick(View v) {
                                 TextView TextOne = (TextView) findViewById(R.id.TestText);
                                 TextOne.setText("Hi");
-                                
+
+                                String host = "127.0.0.1"; // localhost
+                                int port = 15000;
+                                String message = "Test";
+                                DatagramSocket dsocket = null;
+
+                                if (android.os.Build.VERSION.SDK_INT > 9)
+                                {
+                                    StrictMode.ThreadPolicy policy =
+                                            new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                                    StrictMode.setThreadPolicy(policy);
+                                }
+
+                                try {
+                                    // Get the Internet address of the specified host
+                                    InetAddress address = InetAddress.getByName(host);
+
+                                    // wrap a packet
+                                    DatagramPacket packet = new DatagramPacket(
+                                            message.getBytes(),
+                                            message.length(),
+                                            address, port);
+
+                                    // Create a datagram socket, send the packet through it, close it.
+                                    dsocket = new DatagramSocket();
+                                    dsocket.send(packet);
+                                    dsocket.close();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
                             }
                         }
                 );
