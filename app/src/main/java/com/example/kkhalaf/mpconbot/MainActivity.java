1a07bca6a900fd35d90294785a72e21622510c4f
KKLTU <kkhalaf@ltu.edu>
2016-05-17 19:13:35 -0400

    StartV StopV working. But L2Bot is not. Here is start of onStart()

--- app/src/main/java/com/example/kkhalaf/mpconbot/MainActivity.java
+++ app/src/main/java/com/example/kkhalaf/mpconbot/MainActivity.java
@@ -16,127 +16,119 @@ import android.graphics.BitmapFactory;
 public class MainActivity extends AppCompatActivity {
 
     String message;
-    String Response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button StrtBtn = (Button) findViewById(R.id.StartButton);
-        Button StpBtn = (Button) findViewById(R.id.StopButton);
-        //final ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        // video stream control
+        Button StrtVBtn = (Button) findViewById(R.id.StartVButton);
+        Button StpVBtn = (Button) findViewById(R.id.StopVButton);
+
+        // Bot Movement Control
+        Button ForwdBtn = (Button) findViewById(R.id.ForwardButton);
+        Button BackwdBtn = (Button) findViewById(R.id.BackwardButton);
+        Button RightBtn = (Button) findViewById(R.id.RightButton);
+        Button LeftVBtn = (Button) findViewById(R.id.LeftButton);
 
         // Start Button Click
-        StrtBtn.setOnClickListener(
+        StrtVBtn.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        message = "StartVideo";
+                        message = "StartV";
                         SendUdpMsg();
-                    }//onClick
-                }//onClickListener
-        );//setOnClickListener
+                    }
+                }
+        );
 
         // Stop Button Click
-        StpBtn.setOnClickListener(
+        StpVBtn.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        message = "StopVideo";
-                        //SendUdpMsg();
-                    }//onClick
-                }//onClickListener
-        );//setOnClickListener
+                        message = "StopV";
 
-//        // Button Declaration
-//        final TextView TextOne = (TextView) findViewById(R.id.StatusText);
-//        Button FrwrdBtn = (Button) findViewById(R.id.ForwardButton);
-//        Button BkwrdBtn = (Button) findViewById(R.id.BackwardButton);
-//        Button RgtBtn = (Button) findViewById(R.id.RightButton);
-//        Button LftBtn = (Button) findViewById(R.id.LeftButton);
-//
-//        // Forward Button Click
-//        FrwrdBtn.setOnTouchListener(
-//                new Button.OnTouchListener() {
-//                    public boolean onTouch(View v, MotionEvent event) {
-//                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-//                            //Button pressed
-//                            TextOne.setText("Forward");
-//                            message = "Forward";
-//                            SendUdpMsg(message);
-//                            return true;
-//                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
-//                            //Button released
-//                            TextOne.setText("Stop");
-//                            message = "Stop";
-//                            SendUdpMsg(message);
-//                            return true;
-//                        }
-//                        return false;
-//                    }//onTouch
-//                });//setOnTouchListener
-//
-//        // Backward Button Click
-//        BkwrdBtn.setOnTouchListener(
-//                new Button.OnTouchListener() {
-//                    public boolean onTouch(View v, MotionEvent event) {
-//                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-//                            //Button pressed
-//                            TextOne.setText("Backward");
-//                            message = "Backward";
-//                            SendUdpMsg(message);
-//                            return true;
-//                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
-//                            //Button released
-//                            TextOne.setText("Stop");
-//                            message = "Stop";
-//                            SendUdpMsg(message);
-//                            return true;
-//                        }
-//                        return false;
-//                    }//onTouch
-//                });//setOnTouchListener
-//
-//        // Right Button Click
-//        RgtBtn.setOnTouchListener(
-//                new Button.OnTouchListener() {
-//                    public boolean onTouch(View v, MotionEvent event) {
-//                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-//                            //Button pressed
-//                            TextOne.setText("Right");
-//                            message = "Right";
-//                            SendUdpMsg(message);
-//                            return true;
-//                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
-//                            //Button released
-//                            TextOne.setText("Stop");
-//                            message = "Stop";
-//                            SendUdpMsg(message);
-//                            return true;
-//                        }
-//                        return false;
-//                    }//onTouch
-//                });//setOnTouchListener
-//
-//        // Left Button Click
-//        LftBtn.setOnTouchListener(
-//                new Button.OnTouchListener() {
-//                    public boolean onTouch(View v, MotionEvent event) {
-//                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-//                            //Button pressed
-//                            TextOne.setText("Left");
-//                            message = "Left";
-//                            SendUdpMsg(message);
-//                            return true;
-//                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
-//                            //Button released do nothing
-//                            return true;
-//                        }
-//                        return false;
-//                    }//onTouch
-//                });//setOnTouchListener
+                    }
+                }
+        );
+
+        // Forward Button Click
+        ForwdBtn.setOnTouchListener(
+                new Button.OnTouchListener() {
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            //Button pressed
+                            message = "Forward";
+
+                            return true;
+                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                            //Button released
+                            message = "Stop";
+                            return true;
+                        }
+                        return false;
+                    }//onTouch
+                });//setOnTouchListener
+
+        // Backward Button Click
+        BackwdBtn.setOnTouchListener(
+                new Button.OnTouchListener() {
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            //Button pressed
+                            message = "Backward";
+                            return true;
+                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                            //Button released
+                            message = "Stop";
+                            return true;
+                        }
+                        return false;
+                    }//onTouch
+                });//setOnTouchListener
+
+        // Right Button Click
+        RightBtn.setOnTouchListener(
+                new Button.OnTouchListener() {
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            //Button pressed
+                            message = "Right";
+                            return true;
+                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                            //Button released
+                            message = "Stop";
+                            return true;
+                        }
+                        return false;
+                    }//onTouch
+                });//setOnTouchListener
+
+        // Left Button Click
+        LeftVBtn.setOnTouchListener(
+                new Button.OnTouchListener() {
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            //Button pressed
+                            message = "Left";
+                            return true;
+                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                            //Button released do nothing
+                            message = "Stop";
+                            return true;
+                        }
+                        return false;
+                    }//onTouch
+                });//setOnTouchListener
     }
 
-    // This function is responsible for sending a udp packet to a hardCoded IP below. Returns nothing and takes a string(the message) as a parameter.
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+    }
+
     public void SendUdpMsg()
     {
         Thread networkThread = new Thread() {
@@ -167,27 +159,201 @@ public class MainActivity extends AppCompatActivity {
 
                     while (true)
                     {
-                        dsocket.receive(packetReceived); // receive packet
-                        byte[] buff = packetReceived.getData(); // convert packet to byte[]
-                        final Bitmap ReceivedImage = BitmapFactory.decodeByteArray(buff, 0, buff.length); // convert byte[] to image
-                        runOnUiThread(new Runnable()
+                        if(message.equals("StartV"))
                         {
-                            @Override
-                            public void run()
+                            dsocket.receive(packetReceived); // receive packet
+                            byte[] buff = packetReceived.getData(); // convert packet to byte[]
+                            final Bitmap ReceivedImage = BitmapFactory.decodeByteArray(buff, 0, buff.length); // convert byte[] to image
+                            runOnUiThread(new Runnable()
                             {
-                                // this is executed on the main (UI) thread
-                                final ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                                imageView.setImageBitmap(ReceivedImage);
+                                @Override
+                                public void run()
+                                {
+                                    // this is executed on the main (UI) thread
+                                    final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                                    imageView.setImageBitmap(ReceivedImage);
+                                }
+                            });
+
+                            ////////////////////////////////////////////////
+
+//                            // wrap a packet
+//                            packetToSend = new DatagramPacket(
+//                                    "StartV".getBytes(),
+//                                    "StartV".length(),
+//                                    address, port);
+//
+//                            // Create a datagram socket, send the packet through it.
+//                            dsocket = new DatagramSocket();
+//                            dsocket.send(packetToSend);
+
+                            ///////////////////////////////////////////////
+
+                        }
+                        else
+                        {
+                            if (message.equals("Forward") || message.equals("Backward") || message.equals("Left") || message.equals("Stop") || message.equals("Right"))
+                            {
+                                // wrap a packet to send the Stop
+                                packetToSend = new DatagramPacket(
+                                        message.getBytes(),
+                                        message.length(),
+                                        address, port);
+
+                                // Create a datagram socket, send the packet through it.
+                                dsocket = new DatagramSocket();
+                                dsocket.send(packetToSend);
+                                message = "StartV";
                             }
-                        });
-                    }//
-                    //dsocket.close();
+                            else
+                            {
+                                packetToSend = new DatagramPacket(
+                                message.getBytes(),
+                                message.length(),
+                                address, port);
+
+                                // Create a datagram socket, send the packet through it.
+                                dsocket = new DatagramSocket();
+                                dsocket.send(packetToSend);
+                                break; // break the whole thread
+                            }
+                        }
+                    }
+
                 } catch (Exception e) {
                     e.printStackTrace();
-                }//catch
-            }//run
-        };// Networkthread
-        networkThread.start();//networkThread.start()
+                }
+            }
+        };
+        networkThread.start();
     }
-
 }
+
+
+
+
+//package com.example.kkhalaf.mpconbot;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//import android.view.MotionEvent;
+//import java.net.*;
+//import java.lang.Object;
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapFactory;
+//
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    String message;
+//    String Response;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Button StrtBtn = (Button) findViewById(R.id.StartVButton);
+//        Button StpBtn = (Button) findViewById(R.id.StopVButton);
+//        final ImageView imageView = (ImageView)findViewById(R.id.imageView);
+//
+//        // Start Button Click
+//        StrtBtn.setOnClickListener(
+//                new Button.OnClickListener() {
+//                    public void onClick(View v) {
+//                        message = "StartVideo";
+//                        SendUdpMsg();
+//                    }//onClick
+//                }//onClickListener
+//        );//setOnClickListener
+//
+//        // Stop Button Click
+//        StpBtn.setOnClickListener(
+//                new Button.OnClickListener() {
+//                    public void onClick(View v) {
+//                        message = "StopVideo";
+//                        //SendUdpMsg();
+//                    }//onClick
+//                }//onClickListener
+//        );//setOnClickListener
+//    }
+//
+//    // This function is responsible for sending a udp packet to a hardCoded IP below. Returns nothing and takes a string(the message) as a parameter.
+//    public void SendUdpMsg()
+//    {
+//        Thread networkThread = new Thread() {
+//
+//            // No local Host 127.0.0.1 in Android
+//            String host = "192.168.200.3"; // Server's IP
+//            int port = 15000;
+//            DatagramSocket dsocket = null;
+//
+//            public void run() {
+//                try {
+//                    // Get the Internet address of the specified host
+//                    InetAddress address = InetAddress.getByName(host);
+//
+//                    // wrap a packet
+//                    DatagramPacket packetToSend = new DatagramPacket(
+//                            message.getBytes(),
+//                            message.length(),
+//                            address, port);
+//
+//                    // Create a datagram socket, send the packet through it.
+//                    dsocket = new DatagramSocket();
+//                    dsocket.send(packetToSend);
+//
+//                    // Here, I am receiving the response
+//                    byte[] buffer = new byte[65535]; // prepare
+//                    DatagramPacket packetReceived = new DatagramPacket(buffer, buffer.length); // prepare
+//
+//                    while (true)
+//                    {
+//                        if(message == "StartVideo")
+//                        {
+//                            dsocket.receive(packetReceived); // receive packet
+//                            byte[] buff = packetReceived.getData(); // convert packet to byte[]
+//                            final Bitmap ReceivedImage = BitmapFactory.decodeByteArray(buff, 0, buff.length); // convert byte[] to image
+//                            runOnUiThread(new Runnable()
+//                            {
+//                                @Override
+//                                public void run()
+//                                {
+//                                    // this is executed on the main (UI) thread
+//                                    final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//                                    imageView.setImageBitmap(ReceivedImage);
+//                                }
+//                            });
+//                        }
+//                        else
+//                        {
+//                            // wrap a packet
+//                            packetToSend = new DatagramPacket(
+//                                    message.getBytes(),
+//                                    message.length(),
+//                                    address, port);
+//
+//                            // Create a datagram socket, send the packet through it.
+//                            dsocket = new DatagramSocket();
+//                            dsocket.send(packetToSend);
+//                            break;
+//                        }
+//                    }
+//                    //dsocket.close();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }//catch
+//            }//run
+//        };// Networkthread
+//        networkThread.start();//networkThread.start()
+//    }
+//}
+
+
+
+
+
